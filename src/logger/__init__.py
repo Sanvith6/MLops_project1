f4c72a3d6efab16e22ac5f177275411f1c2ab1f2
SanvithJS <jssanvith@gmail.com>
2025-08-02 00:06:39 +0530

    logging and exception added

---
+++ src/logger/__init__.py
@@ -0,0 +1,37 @@
+import logging
+import os
+from logging.handlers import RotatingFileHandler
+from from_root import from_root
+from datetime import datetime
+
+# Constants
+LOG_DIR = 'logs'
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+MAX_LOG_SIZE = 5 * 1024 * 1024
+BACKUP_COUNT = 3
+
+# Create logs directory using from_root
+log_dir_path = os.path.join(from_root(), LOG_DIR)
+os.makedirs(log_dir_path, exist_ok=True)
+log_file_path = os.path.join(log_dir_path, LOG_FILE)
+
+def configure_logger():
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter("[ %(asctime)s ] %(name)s - %(levelname)s - %(message)s")
+
+    file_handler = RotatingFileHandler(log_file_path, maxBytes=MAX_LOG_SIZE, backupCount=BACKUP_COUNT)
+    file_handler.setFormatter(formatter)
+    file_handler.setLevel(logging.DEBUG)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+    console_handler.setLevel(logging.DEBUG)
+
+    if not logger.hasHandlers():
+        logger.addHandler(file_handler)
+        logger.addHandler(console_handler)
+
+# Call once when module is imported
+configure_logger()
